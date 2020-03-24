abb963188763facbe5dc8158e5fdbd8f33dc3adf
Hayden Daly <hdaly1@stevens.edu>
2020-03-24 11:38:16 -0700

    homescreen transition - still needs fade

--- screens/getStarted.js
+++ screens/getStarted.js
@@ -1,38 +1,54 @@
 import React, { useState } from "react";
-import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity, ImageBackground, Animated } from "react-native";
 
 export default function GetStarted({ navigation }) {
   const pressHandler = () => {
     navigation.push("PhoneInput");
   };
+  const imageHolder = [require('../assets/getStarted0.jpg'), require('../assets/getStarted1.jpg')]
+  const [currentImageIndex, setCurrentImageIndex] = useState(0)
+  setTimeout(() => {
+    setCurrentImageIndex(currentImageIndex == imageHolder.length - 1 ?
+          0:
+          currentImageIndex + 1
+        )
+  }, 5000)
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.exireText}>exire</Text>
-      <Text>Going out made simple</Text>
-      <View style={styles.bottom}>
-        <TouchableOpacity
-          style={styles.getStartedButton}
-          activeOpacity={0.75}
-          onPress={pressHandler}
-        >
-          <Text style={styles.buttonText}>Get Started</Text>
-        </TouchableOpacity>
+    <ImageBackground source={imageHolder[currentImageIndex]} style={{width: '100%', height: '100%'}}>
+      <View style={styles.container}>
+        <Text style={styles.headerText}>exire</Text>
+        <Text style={styles.subHeaderText}>Going out made simple</Text>
+        <View style={styles.bottom}>
+          <TouchableOpacity
+            style={styles.getStartedButton}
+            activeOpacity={0.75}
+            onPress={pressHandler}
+          >
+            <Text style={styles.buttonText}>Get Started</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#eee",
+    backgroundColor: 'rgba(0,0,0,0.15)',
     alignItems: "center"
   },
-  exireText: {
+  headerText: {
     marginTop: 75,
-    fontSize: 42,
-    color: "#007aff"
+    fontSize: 52,
+    color: "#007aff",
+    fontWeight: "bold"
+  },
+  subHeaderText: {
+    fontSize: 24,
+    color: "#fff",
+    fontWeight: "bold"
   },
   getStartedButton: {
     backgroundColor: "#007aff",
