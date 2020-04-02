81285f3a2676bee2e1aa8e3b36850286c91b7acc
Frank Pinnola <frankie4pinnola@gmail.com>
2020-04-02 12:50:33 -0400

    Cleared Uneccesary logs

--- screens/LoadingScreen.js
+++ screens/LoadingScreen.js
@@ -1,54 +1,45 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import users from '../functions/users';
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import users from "../functions/users";
 
-import firebase from 'firebase';
+import firebase from "firebase";
 
 export default class LoadingScreen extends Component {
-  componentDidMount(){
-    this.checkIfLoggedIn()
+  componentDidMount() {
+    this.checkIfLoggedIn();
   }
 
   checkIfLoggedIn = () => {
-    firebase.auth().onAuthStateChanged((user) =>
-    {
-
-
-      console.log(user);
-      if (user)
-      {
+    firebase.auth().onAuthStateChanged(user => {
+      // console.log(user);
+      if (user) {
         //TODO: Get User from database, check if exists
         // console.log(firebase.auth().currentUser);
-        users.doesExist(firebase.auth().currentUser.uid, (exists) => {
-          console.log(exists);
+        users.doesExist(firebase.auth().currentUser.uid, exists => {
+          // console.log(exists);
           if (exists) {
             //User already exists, has selected Categories
-            this.props.navigation.navigate('ChatStack');
+            this.props.navigation.navigate("ChatStack");
           } else {
-            this.props.navigation.navigate('CategoryPreference');
+            this.props.navigation.navigate("CategoryPreference");
           }
         });
+      } else {
+        this.props.navigation.navigate("GetStarted");
       }
-      else {
-        this.props.navigation.navigate('GetStarted');
-      }
-    }
-  );
-};
+    });
+  };
 
   render() {
-    return (
-      <View style={styles.container}>
-      </View>
-    );
+    return <View style={styles.container}></View>;
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     backgroundColor: "#007AFF"
   }
 });
