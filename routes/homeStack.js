097e6dfcdba3664544c7fd45a2f32f849b23f3b7
Frank Pinnola <frankie4pinnola@gmail.com>
2020-05-12 23:37:14 -0400

    Tab Bar UI updates

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -1,28 +1,44 @@
-import { createStackNavigator, TransitionPresets } from 'react-navigation-stack';
-import { createAppContainer } from 'react-navigation';
-import { createMaterialTopTabNavigator } from 'react-navigation-tabs';
-import React from 'react';
-import { View, TouchableOpacity, ImageBackground, AsyncStorage } from 'react-native';
+import {
+  createStackNavigator,
+  TransitionPresets,
+} from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+import { createMaterialTopTabNavigator } from "react-navigation-tabs";
+import React from "react";
+import {
+  View,
+  TouchableOpacity,
+  ImageBackground,
+  AsyncStorage,
+  Image,
+} from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
+
 // import Exire from "../assets/icons/exire.svg"
 
 import { navigationStyles } from "../global/navigationStyles";
 import { shadowStyles } from "../global/shadowStyles";
-import Discover from '../screens/Discover';
-import Plans from '../screens/Plans';
-import Chats from '../screens/Chats';
+import Discover from "../screens/Discover";
+import Plans from "../screens/Plans";
+import Chats from "../screens/Chats";
+import Exire from "../assets/icons/exire.svg";
+
+const exire = require("../assets/icons/exire.svg");
 
 const screens = {
   Discover: {
     screen: Discover,
-    navigationOptions: ({navigation}) => {
+    navigationOptions: ({ navigation }) => {
       return {
         title: "",
         tabBarIcon: ({ tintColor }) => (
-          <Icon name="compass" size={45} color={tintColor} style={{paddingLeft: 45}} />
+          <Image
+            style={{ width: 42, height: 42, tintColor: tintColor }}
+            source={require("../assets/icons/compass.png")}
+          />
         ),
-      }
-    }
+      };
+    },
   },
   Plans: {
     screen: Plans,
@@ -30,11 +46,32 @@ const screens = {
       return {
         title: "",
         tabBarIcon: ({ tintColor }) => (
-          <Icon name="home" size={45} color={tintColor} />
-          // <Exire />
+          // <Exire style={{ height: 24, width: 24,  }} />
+          // <Icon
+          //   name="home"
+          //   size={36}
+          //   color={tintColor}
+          //   style={{ paddingRight: 45, marginTop: -5 }}
+          // />
+          <Image
+            style={{ width: 36, height: 36, tintColor: tintColor }}
+            source={require("../assets/icons/exire.png")}
+          />
         ),
-      }
-    }
+        //   <Icon
+        //   name="comment"
+        //   size={36}
+        //   color={tintColor}
+        //   style={{ paddingRight: 45, marginTop: -5 }}
+        // />
+        // <Image
+        //   sytle={{ width: 24, height: 24 }}
+        //   size={24}
+        //   source={require("../assets/icons/exire.svg")}
+        // />
+        // <Exire />
+      };
+    },
   },
   Chats: {
     screen: Chats,
@@ -42,34 +79,41 @@ const screens = {
       return {
         title: "",
         tabBarIcon: ({ tintColor }) => (
-            <Icon name="comment" size={45} color={tintColor} style={{paddingRight: 45, marginTop: -5}}/>
+          <Image
+            style={{ width: 36, height: 36, tintColor: tintColor }}
+            source={require("../assets/icons/chat.png")}
+          />
         ),
-      }
-    }
-  }
-}
+      };
+    },
+  },
+};
 
 const HomeTab = createMaterialTopTabNavigator(screens, {
-  initialRouteName: 'Plans',
-  tabBarPosition: 'bottom',
+  initialRouteName: "Plans",
+  tabBarPosition: "bottom",
   defaultNavigationOptions: {
-    ...TransitionPresets.FadeFromBottomAndroid
+    ...TransitionPresets.FadeFromBottomAndroid,
   },
+  swipeEnabled: false,
   tabBarOptions: {
-    activeTintColor : '#3597e9',
-    inactiveTintColor: '#6dbabe',
+    activeTintColor: "#3597e9",
+    inactiveTintColor: "#aaa",
     style: [shadowStyles.shadowUp, navigationStyles.footer],
     indicatorStyle: {
-      height: 0
+      height: 0,
     },
     showIcon: true,
-    iconStyle: [shadowStyles.shadowDown, {
-      height: 45,
-      width: 110,
-      alignItems: 'center',
-      shadowRadius: .3
-    }]
-  }
+    iconStyle: [
+      shadowStyles.shadowDown,
+      {
+        height: 45,
+        width: 110,
+        alignItems: "center",
+        shadowRadius: 0.3,
+      },
+    ],
+  },
 });
 
 const HomeStack = createStackNavigator({
@@ -78,23 +122,26 @@ const HomeStack = createStackNavigator({
     navigationOptions: ({ navigation }) => {
       return {
         headerShown: true,
-        title: ['Discover', 'Plans', 'Chats'][navigation.state.index],
+        title: ["Discover", "Plans", "Chats"][navigation.state.index],
         headerStyle: [shadowStyles.shadowDown, navigationStyles.header],
         headerTitleStyle: navigationStyles.headerTitle,
         headerTitleAlign: "center",
         headerLeft: () => (
-          <TouchableOpacity onPress={() => console.log(navigation)} style={navigationStyles.icon}>
+          <TouchableOpacity
+            onPress={() => console.log(navigation)}
+            style={navigationStyles.icon}
+          >
             <Icon
-              name='user-circle'
-              color='#FFF'
+              name="user-circle"
+              color="#FFF"
               size={32}
               style={navigationStyles.shadowDown}
             />
           </TouchableOpacity>
-        )
+        ),
       };
     },
-  }
-})
+  },
+});
 
 export default createAppContainer(HomeStack);
