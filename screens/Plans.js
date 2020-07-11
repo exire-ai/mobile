67c5ccf5776a8e47462eaefc07e3c4ccccbc1fab
Frank Pinnola <frankie4pinnola@gmail.com>
2020-07-10 20:58:59 -0400

    Accept Notification Payloads

--- screens/Plans.js
+++ screens/Plans.js
@@ -11,6 +11,7 @@ import { useNavigationFocus, NavigationEvents } from "react-navigation";
 import * as Permissions from "expo-permissions";
 import Constants from "expo-constants";
 import { Notifications } from "expo";
+// import * as Notifications from "expo-notifications";
 
 import Plan from "../components/Plan";
 
@@ -32,7 +33,7 @@ export default class Plans extends Component {
       data: null,
       refreshing: false,
       expoPushToken: "",
-      onboard: 'false'
+      onboard: "false",
     };
   }
 
@@ -56,7 +57,7 @@ export default class Plans extends Component {
       token = await Notifications.getExpoPushTokenAsync();
       AsyncStorage.getItem("userID").then((userID) => {
         users.updateExpoPushToken(userID, token, (res) => {
-          AsyncStorage.setItem('token', token);
+          AsyncStorage.setItem("token", token);
         });
       });
 
@@ -78,12 +79,23 @@ export default class Plans extends Component {
   componentDidMount() {
     this.props.navigation.addListener("willFocus", this.loadData);
     this.registerForPushNotificationsAsync();
+    Notifications.addListener((notification) => {
+      if (notification.origin === "received") {
+        console.log("App open already");
+        return;
+      }
+      let data = notification.data;
+      if (data.action === "open-chat") {
+        this.props.navigation.navigate("Chats", { toOpen: data.token });
+      }
+    });
+
     this.loadData();
-    AsyncStorage.getItem("onboard").then(onboard => {
+    AsyncStorage.getItem("onboard").then((onboard) => {
       this.setState({
-        onboard: onboard
-      })
-    })
+        onboard: onboard,
+      });
+    });
   }
 
   loadData = () => {
@@ -130,11 +142,11 @@ export default class Plans extends Component {
         <View style={[plansStyles.container, { alignItems: "flex-start" }]}>
           <NavigationEvents
             onDidFocus={() => {
-              AsyncStorage.getItem("onboard").then(onboard => {
+              AsyncStorage.getItem("onboard").then((onboard) => {
                 this.setState({
-                  onboard: onboard
-                })
-              })
+                  onboard: onboard,
+                });
+              });
             }}
           />
           <View
@@ -143,7 +155,10 @@ export default class Plans extends Component {
               alignItems: "center",
               height: "100%",
               justifyContent: "center",
-              backgroundColor: this.state.onboard != 'false' ? "rgba(0,0,0,.3)" : "rgba(0,0,0,0)"
+              backgroundColor:
+                this.state.onboard != "false"
+                  ? "rgba(0,0,0,.3)"
+                  : "rgba(0,0,0,0)",
             }}
           >
             <View
@@ -164,7 +179,9 @@ export default class Plans extends Component {
                   { width: "100%", textAlign: "center" },
                 ]}
               >
-                { this.state.onboard != 'false' ? "Welcome to Exire" : "Create A Plan" }
+                {this.state.onboard != "false"
+                  ? "Welcome to Exire"
+                  : "Create A Plan"}
               </Text>
               <Text
                 style={[
@@ -172,7 +189,9 @@ export default class Plans extends Component {
                   { width: "100%", textAlign: "center", marginTop: 10 },
                 ]}
               >
-                { this.state.onboard != 'false' ? "Let's take a tour of the app to get you familiar with how it works!" : "Create your first plan through the discover on the left or through conversation on the right."}
+                {this.state.onboard != "false"
+                  ? "Let's take a tour of the app to get you familiar with how it works!"
+                  : "Create your first plan through the discover on the left or through conversation on the right."}
               </Text>
               <TouchableOpacity
                 activeOpacity={0.9}
@@ -199,7 +218,9 @@ export default class Plans extends Component {
                     },
                   ]}
                 >
-                  { this.state.onboard != 'false' ? "Let's Go!" : "Explore Experiences" }
+                  {this.state.onboard != "false"
+                    ? "Let's Go!"
+                    : "Explore Experiences"}
                 </Text>
               </TouchableOpacity>
             </View>
@@ -221,7 +242,9 @@ export default class Plans extends Component {
                 this.loadData();
               }}
               refreshing={this.state.refreshing}
-              keyExtractor={(item, index) => "name" + item.title + item.start_time}
+              keyExtractor={(item, index) =>
+                "name" + item.title + item.start_time
+              }
               renderItem={({ item, index }) => (
                 <Plan data={item} onTap={this.planTapped.bind(this)} />
               )}
